387411f9aed263c2d5b76ab672f0d03e056d9591
Roricondesu <Roricondesu@users.noreply.github.com>
2026-07-15 05:18:16 +0000

    fix: 深色背景右侧20px圆角，lime左边缘呈向内收圆角矩形

--- src/components/common/BeatmapCard.tsx
+++ src/components/common/BeatmapCard.tsx
@@ -178,7 +178,7 @@ export const BeatmapCard: React.FC<BeatmapCardProps> = React.memo(({ set, index 
             position: "absolute",
             top: 0, left: 0, bottom: 0,
             right: hover && !isLoadedSet(set) && !isDownloaded ? 38 : 0,
-            borderRadius: 10,
+            borderRadius: "10px 20px 20px 10px",
             overflow: "hidden",
             zIndex: 2,
             transition: "right 0.3s cubic-bezier(0.22,1,0.36,1)",
